29b055382cdaa07920a692723621905f6164b6b5
nakuul40 <nakul.ravekar@gmail.com>
2018-08-02 14:58:23 +0530

    End if Section 5

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log('Logged In Succesfully');
     }, error => {
-      console.log('Failed to Logged In');
+      console.log(error);
     });
   }
 
--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -44,7 +44,8 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
 
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto) 
-        {
+        {            
+            
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(),userForLoginDto.Password);
 
             if(userFromRepo==null)
@@ -73,10 +74,8 @@ namespace DatingApp.API.Controllers
                 {
                     token = tokenHandler.WriteToken(token)
                 }
-            );
-            
-
-
-        }
+            ); 
+        }          
+        
     }
 }
